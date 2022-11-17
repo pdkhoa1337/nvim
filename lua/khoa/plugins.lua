e6e755a1c4abe1ebd89bb85ab7be8d15e7ce2632
Khoa Phan <khoa.phan@floatingcube.com>
2022-11-17 11:09:40 +0700

    bootstrap packer

--- lua/khoa/plugins.lua
+++ lua/khoa/plugins.lua
@@ -1,5 +1,21 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
+local fn = vim.fn
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 vim.cmd([[
@@ -68,4 +84,9 @@ return packer.startup(function(use)
   use 'f-person/git-blame.nvim'
 
 
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
